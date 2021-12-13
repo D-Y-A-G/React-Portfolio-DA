f9a0b64c108f9bda6c82302ee9944f40b2e18c75
Denis Arce <dnsarce21@gmail.com>
2021-12-13 11:43:06 -0600

    added react files for boiler plate with npx create-react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="portfolio">
+      <h1>Denis Arce's REACT Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
